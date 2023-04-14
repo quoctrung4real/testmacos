48067f4a380e43c2fe8bfbaacf0c5f66def314a9
quoctrung4real <quoctrung4real@gmail.com>
2023-04-14 20:50:42 +0700

    bai8

---
+++ ss8_Java_Script/thuc_hanh/prompt_bai3.js
@@ -0,0 +1,4 @@
+let a = +prompt("Nhap vao gia tri a");
+let b = +prompt("Nhap vao gia tri b");
+let Tong = a+b
+document.write("Tong cua a va b =" + Tong)
---
+++ ss8_Java_Script/thuc_hanh/sudung_toantu.js
@@ -0,0 +1,8 @@
+let inputHeight;
+let inputWidth;
+inputWidth = prompt("Nhap vao chieu dai")
+inputHeight = prompt("Nhap vao chieu cao")
+let height = parseInt(inputHeight)
+let width = parseInt(inputWidth)
+let dientich = height*width;
+document.write("Dien tich bang =" + dientich)
